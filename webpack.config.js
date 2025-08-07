4bb28ba5a7ab9afb6e72d9e2a8897d1d8fe7f26b
陨落基围虾 <3161880837@qq.com>
2025-08-07 12:50:19 +0800

    refactor: 更新项目名称并重构webpack配置
    - 将项目名称从'fs-context'改为'example-extension'
    - 重构webpack配置，添加entry、output、module和plugins配置
    - 新增devServer配置并集成Webpackbar插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,47 @@
 const path = require("path");
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Webpackbar = require("webpackbar");
 
-const tsconfig = require("./tsconfig.json");
+const tsconfigJson = require("./tsconfig.json");
+const packageJson = require("./package.json");
 
 /**
  * @type {import('webpack').Configuration}
  */
 module.exports = {
+    entry: "@/extension.ts",
     resolve: {
         extensions: [".js", ".ts"],
         alias: Object.fromEntries(
-            Object.entries(tsconfig.compilerOptions.paths)
+            Object.entries(tsconfigJson.compilerOptions.paths)
                 .map(([key, value]) => [key.replace("/*", ""), path.resolve(__dirname, value[0].replace("/*", ""))])
         )
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: `${packageJson.name}.js`
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/i,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new Webpackbar({
+            name: packageJson.name,
+            color: "green"
+        })
+    ],
+    devServer: {
+        port: 25565,
+        setupExitSignals: false,
+        client: {
+            logging: "none"
+        },
+        webSocketServer: false
     }
 };
