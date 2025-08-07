5feca2dd6a7b71b4ad8e17d00f9e141a7207715f
陨落基围虾 <3161880837@qq.com>
2025-08-07 12:19:58 +0800

    chore: 删除项目文件及代码库
    移除所有项目相关文件，包括源代码、配置文件、文档及资源文件，准备重构v2

--- src/fs-context/built-ins/registers.ts
+++
@@ -1,5 +0,0 @@
-import type { ExtensionRegister } from "@framework/internal";
-export const TurboWarp: ExtensionRegister = (metadata, scratch) => {
-    scratch.extensions.register(new metadata.constructors.generated(scratch));
-};
-export const GandiIDE: ExtensionRegister = TurboWarp;
