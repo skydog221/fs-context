b4c8b37a438e35430faa344b160938dd73e54add
陨落基围虾 <3161880837@qq.com>
2025-08-08 17:44:18 +0800

    refactor(fs-context): 移除调试用的console.log语句
    清理代码中不再需要的调试输出，保持代码整洁

--- src/fs-context/entry.ts
+++ src/fs-context/entry.ts
@@ -2,7 +2,6 @@ import extension from "@/extension";
 import { extensionManager } from "fs-context";
 
 console.log(`Loading extension ${fsContext.extension.name}@${fsContext.extension.author}-v${fsContext.extension.version} on platform "${fsContext.platform}".`);
-console.log(fsContext);
 
 const env = extensionManager.createContextEnvironment(extension);
 if (fsContext.developing) {
