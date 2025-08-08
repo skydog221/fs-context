21f2bdcd588bfefe45efa36342966e0b1db0d16a
陨落基围虾 <3161880837@qq.com>
2025-08-08 18:17:15 +0800

    style: 修正控制台日志中sandboxed的大小写格式

--- src/fs-context/manager/extensions.ts
+++ src/fs-context/manager/extensions.ts
@@ -59,7 +59,7 @@ export function load(environment: ContextEnvironment, platform: string) {
         const runtime = obtainRuntime(environment, platform, ...contextData);
         const isSandboxed = pluginManager.call(platform, "isSandboxed", [environment, runtime]).data;
         if (fsContext.developing) {
-            console.log(`Runtime(${isSandboxed ? "S" : "Uns"}andboxed) obtained:`, runtime);
+            console.log(`Runtime(${isSandboxed ? "s" : "uns"}andboxed) obtained:`, runtime);
         }
         if (!environment.extension.metadata.allowSandbox && isSandboxed) {
             throw new Error(`Extension "${environment.extension.metadata.name}" doesn't allow sandboxed, but ${platform} is sandboxed.`);
