cbd75fc240d18d3e8e983e76c614c31fe1dae35b
陨落基围虾 <3161880837@qq.com>
2025-08-09 08:12:55 +0800

    refactor(blocks): 移除add.ts中的多余空行并格式化代码
    清理代码中的多余空行以提高可读性，保持代码风格一致

--- src/extension/blocks/add.ts
+++ src/extension/blocks/add.ts
@@ -2,9 +2,4 @@ import { blockType } from "fs-context";
 
 export default blockType.reporter("add")
     .text('Add [   a   :       "number"   ] to [b=123] and [c:color=#ff0000] and [d:bool]')
-
-
-
-
-
     .action(args => args.a + args.b + args.c + args.d);
