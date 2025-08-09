f3eb233f9494ba5398e6a295a298b1b67d406fb5
陨落基围虾 <3161880837@qq.com>
2025-08-10 06:31:55 +0800

    feat(extension): 添加猜盐扩展的猜病功能块
    添加新的猜病功能块到猜盐扩展中，支持日期、消息和chatId参数

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -1,6 +1,12 @@
-import { blockType, extension, numberParser } from "fs-context";
-import { ArgumentMap } from "fs-context/structs/parser/compiltime";
+import { blockType, extension } from "fs-context";
 
-export default extension("fourcalc")
-    .name("四则运算")
-    .loader("numberArray", (src) => src.split(" ").map(numberParser.toNumber))
+export default extension("caiyan")
+    .name("猜盐")
+    .block(
+        blockType.reporter("guessDisease")
+            .text("猜病|日期：[DATE=0101]消息：[MESSAGE=Hello]chatId:[CHATID]")
+            .action(args => {
+                console.log(args);
+            })
+            .build()
+    )
