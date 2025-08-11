516e9a0f35a2181e80e09715e9ca671bf6bffd0f
陨落基围虾 <3161880837@qq.com>
2025-08-11 09:52:49 +0800

    fix: 将CHATID参数重命名为APPLE以保持一致性
    修改猜病功能中的参数命名，从CHATID改为APPLE，使参数命名与实际使用保持一致

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -3,9 +3,9 @@ import { blockType, extension } from "fs-context";
 export default extension()
     .block(
         blockType.reporter("guessDisease")
-            .text("猜病|日期：[DATE=0101]消息：[MESSAGE=Hello]chatId:[CHATID]")
+            .text("猜病|日期：[DATE=0101]消息：[MESSAGE=Hello]chatId:[APPLE]")
             .action(args => {
-                console.log(args);
+                console.log(args.APPLE);
             })
             .build()
     )
