7c4dcf1ff4fcb3c122be555facb157d7a94f871a
skydog221 <MoreBugOfDog@outlook.com>
2025-08-26 20:54:41 +0800

    fix:修复chalk-template导致的编译问题
    -移除chalk-template，改为使用chalk

--- src/native/plugins.ts
+++ src/native/plugins.ts
@@ -1,49 +1,78 @@
-import { Linter } from 'eslint';
-import fs from 'fs';
-import { ExtendWebpackConfig, NativePlugin } from './structs/plugin';
-import path from 'path';
-import { Configuration } from 'webpack';
-import chalkTemplate from 'chalk-template';
+import { Linter } from "eslint";
+import fs from "fs";
+import { ExtendWebpackConfig, NativePlugin } from "./structs/plugin";
+import path from "path";
+import { Configuration } from "webpack";
+import chalk from "chalk";
 
 export function load() {
-    console.log(`Mode: ${process.env.NODE_ENV === 'production' ? 'production' : 'development'}`);
-    const disablePlugins = JSON.parse(fs.readFileSync(path.join(__dirname, '../../plugins/disable.json'), 'utf-8')) as Record<'platform' | 'folder', string[]>;
-    const webpack: Record<string, (config: ExtendWebpackConfig) => Configuration> = {};
-    const eslint: Linter.Config[] = [];
-    const pluginsDir = path.join(__dirname, '../../plugins');
-    const pluginFolders = fs.readdirSync(pluginsDir)
-        .filter(file => fs.statSync(path.join(pluginsDir, file)).isDirectory());
-    const pluginIds: string[] = [];
-    function pad(id: string) {
-        return id.padEnd(Math.max(...pluginIds.map(id => id.length)), ' ');
+  console.log(
+    `Mode: ${
+      process.env.NODE_ENV === "production" ? "production" : "development"
+    }`
+  );
+  const disablePlugins = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "../../plugins/disable.json"), "utf-8")
+  ) as Record<"platform" | "folder", string[]>;
+  const webpack: Record<
+    string,
+    (config: ExtendWebpackConfig) => Configuration
+  > = {};
+  const eslint: Linter.Config[] = [];
+  const pluginsDir = path.join(__dirname, "../../plugins");
+  const pluginFolders = fs
+    .readdirSync(pluginsDir)
+    .filter((file) => fs.statSync(path.join(pluginsDir, file)).isDirectory());
+  const pluginIds: string[] = [];
+  function pad(id: string) {
+    return id.padEnd(Math.max(...pluginIds.map((id) => id.length)), " ");
+  }
+  for (const folder of pluginFolders) {
+    pluginIds.push(folder);
+  }
+  for (const folder of pluginFolders) {
+    const currentNativeId = folder;
+    if (disablePlugins.folder.includes(currentNativeId)) {
+      console.log(
+        `${chalk.gray(pad(currentNativeId))} | ${chalk.red("DISABLED")}`
+      );
+      continue;
     }
-    for (const folder of pluginFolders) {
-        pluginIds.push(folder);
-    }
-    for (const folder of pluginFolders) {
-        const currentNativeId = folder;
-        if (disablePlugins.folder.includes(currentNativeId)) {
-            console.log(chalkTemplate`{gray ${pad(currentNativeId)}} | {red DISABLED}`);
-            continue;
+    try {
+      const nativePath = path.resolve(
+        "dist/native/plugins",
+        folder,
+        "native.js"
+      );
+      if (fs.existsSync(nativePath)) {
+        const {
+          default: plugin,
+        }: { default: NativePlugin } = require(nativePath);
+        if (disablePlugins.platform.includes(plugin.platform)) {
+          console.log(
+            `${chalk.gray(pad(plugin.platform))} | ${chalk.red("DISABLED")}`
+          );
+          continue;
         }
-        try {
-            const nativePath = path.resolve('dist/native/plugins', folder, 'native.js');
-            if (fs.existsSync(nativePath)) {
-                const { default: plugin }: { default: NativePlugin } = require(nativePath);
-                if (disablePlugins.platform.includes(plugin.platform)) {
-                    console.log(chalkTemplate`{gray ${pad(plugin.platform)}} | {red DISABLED}`);
-                    continue;
-                }
-                webpack[currentNativeId] = plugin.configureWebpack?.call(plugin) ?? (() => ({}));
-                eslint.push(...(plugin.configureESLint?.call(plugin) ?? []));
-                console.log(chalkTemplate`{gray ${pad(currentNativeId)}} | {green loaded successfully}: {cyan ${plugin.platform}}.`);
-            } else {
-                webpack[currentNativeId] = () => ({});
-                console.warn(chalkTemplate`{gray ${pad(currentNativeId)}} | {yellow not found native module}.`);
-            }
-        } catch (err) {
-            console.error(chalkTemplate`{gray ${pad(currentNativeId)}} | {red ${err}}`);
-        }
+        webpack[currentNativeId] =
+          plugin.configureWebpack?.call(plugin) ?? (() => ({}));
+        eslint.push(...(plugin.configureESLint?.call(plugin) ?? []));
+        console.log(
+          `${chalk.gray(pad(currentNativeId))} | ${chalk.green(
+            "loaded successfully"
+          )}: ${chalk.cyan(plugin.platform)}.`
+        );
+      } else {
+        webpack[currentNativeId] = () => ({});
+        console.warn(
+          `${chalk.gray(pad(currentNativeId))} | ${chalk.yellow(
+            "not found native module"
+          )}.`
+        );
+      }
+    } catch (err) {
+      console.error(`${chalk.gray(pad(currentNativeId))} | ${chalk.red(err)}`);
     }
-    return { webpack, eslint };
+  }
+  return { webpack, eslint };
 }
